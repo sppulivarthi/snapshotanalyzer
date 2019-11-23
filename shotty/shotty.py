644490e74d454da3627d150c0fec2a95cd3cf4eb
Suresh Pulivarthi <suresh.pulivarthi@helioassociates.io>
2019-11-23 16:43:10 -0600

    Show all snapshots in list if --all is given

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -24,7 +24,9 @@ def snapshots():
 @snapshots.command('list')
 @click.option('--project', default=None,
     help="Only snapshots for project (tag Project:<name>)")
-def list_volumes(project):
+@click.option('--all', 'list_all', default=False, is_flag=True,
+    help="List all snapshots for each volume, not just the most recent")
+def list_snapshots(project, list_all):
     "List EC2 snapshots"
     instances = filter_instances(project)
     for i in instances:
@@ -38,8 +40,8 @@ def list_volumes(project):
                     s.progress,
                     s.start_time.strftime("%c")
                 )))
+                if s.state == 'completed' and not list_all: break
     return
-
 
 @cli.group('volumes')
 def volumes():
