27e404f048cd40e62c5b23d478e15b38dd073cbc
Suresh Pulivarthi <suresh.pulivarthi@helioassociates.io>
2019-11-23 15:48:05 -0600

    Hnadle error from AWS when stopping a pending instance or starting a stopping one

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import click
 
 session = boto3.Session(profile_name='shotty')
@@ -113,7 +114,11 @@ def stop_instances(project):
 
     for i in instances:
         print("stopping {0} ...".format(i.id))
-        i.stop()
+        try:
+            i.stop()
+        except botocore.exceptions.ClientError as e:
+            print("Cound not stop {0}.".format(i.id) + str(e))
+            continue
     return
 
 @instances.command('start')
@@ -125,7 +130,11 @@ def start_instances(project):
 
     for i in instances:
         print("starting {0} ...".format(i.id))
-        i.start()
+        try:
+            i.start()
+        except botocore.exceptions.ClientError as e:
+            print("Cound not start {0}.".format(i.id) + str(e))
+            continue
     return
 
 if __name__ == '__main__':
